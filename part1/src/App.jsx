8951a73a0b802c7b581d7270cfe97df49d08887d
tpantsar <tomi.pantsar@gmail.com>
2024-09-02 14:34:03 +0300

    part 1d 1.6 uniface step1 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,59 +1,55 @@
 /* eslint-disable react/prop-types */
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+import { useState } from "react";
+
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
 };
 
-const Content = ({ parts }) => {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Display = ({ text, counter }) => {
   return (
     <div>
-      {parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
+      {text} {counter}
     </div>
   );
 };
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
-  );
-};
-
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const header = "Give feedback";
+  const statistics = "Statistics";
+
+  const increaseGoodByOne = () => {
+    console.log("clicked good button");
+    setGood(good + 1);
+  };
+
+  const increaseNeutralByOne = () => {
+    console.log("clicked neutral button");
+    setNeutral(neutral + 1);
+  };
+
+  const increaseBadByOne = () => {
+    console.log("clicked bad button");
+    setBad(bad + 1);
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header title={header} />
+      <Button handleClick={increaseGoodByOne} text="good" />
+      <Button handleClick={increaseNeutralByOne} text="neutral" />
+      <Button handleClick={increaseBadByOne} text="bad" />
+      <Header title={statistics} />
+      <Display text="good" counter={good} />
+      <Display text="neutral" counter={neutral} />
+      <Display text="bad" counter={bad} />
     </div>
   );
 };
---
+++ part1/src/examples/App.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
