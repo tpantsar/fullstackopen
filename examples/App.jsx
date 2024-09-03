e61d56b731bf021dc8d1211321d5379921b1d051
Tomi Pantsar <tomi___00@hotmail.com>
2024-09-03 12:49:53 +0300

    init new osa2 react project

---
+++ examples/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => {
+    console.log("clicked");
+    setCounter(counter + 1);
+  };
+
+  const setToZero = () => {
+    console.log("clicked");
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+    </div>
+  );
+};
+
+export default App;
