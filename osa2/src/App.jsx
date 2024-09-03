f60a2e3d5a6335ab282685a1ed8a8833199a61b9
Tomi Pantsar <tomi___00@hotmail.com>
2024-09-03 16:07:53 +0300

    part2 step4 done

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
+const Header = ({ title }) => {
+  return <h2>{title}</h2>;
+};
+
 const Person = ({ person }) => {
-  return <li>{person.content}</li>;
+  return (
+    <li>
+      {person.name} {person.number}
+    </li>
+  );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -16,36 +31,64 @@ const App = () => {
       alert("Name cannot be empty");
       return;
     }
-    if (persons.some((person) => person.content === newName)) {
+    if (newNumber.length === 0) {
+      alert("Number cannot be empty");
+      return;
+    }
+    if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
     const personObject = {
       id: persons.length + 1,
-      content: newName,
+      name: newName,
+      number: newNumber,
     };
 
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
+
+    console.log("persons", persons);
   };
 
   const handleNewPerson = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const personFilter = persons.filter((person) =>
+    person.name.toLowerCase().includes(nameFilter.toLowerCase())
+  );
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Header title="Phonebook" />
+      <div>
+        filter shown with
+        <input
+          value={nameFilter}
+          onChange={(e) => setNameFilter(e.target.value)}
+        />
+      </div>
+      <h3>Add new person</h3>
       <form onSubmit={addPerson}>
         <div>
           name:
           <input value={newName} onChange={handleNewPerson} />
-          <button type="submit">add</button>
         </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <button type="submit">add</button>
       </form>
-      <h2>Numbers</h2>
+      <Header title="Numbers" />
       <ul>
-        {persons.map((person) => (
+        {personFilter.map((person) => (
           <Person key={person.name} person={person} />
         ))}
       </ul>
