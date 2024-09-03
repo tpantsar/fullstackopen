0ff66390e8a51b339a5eac60f1f067a85357cc21
Tomi Pantsar <tomi___00@hotmail.com>
2024-09-03 11:43:28 +0300

    Change repository structure

---
+++ osa1/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./kurssitiedot/App";
+// import App from "./unicafe/App";
+// import App from "./anekdootit/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
