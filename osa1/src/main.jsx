674cd72c44c969a24beb9d4cd67a44bb8e792a8d
tpantsar <tomi.pantsar@gmail.com>
2024-09-13 11:40:03 +0300

    refactor part0 and part1 to separate projects

--- osa1/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./kurssitiedot/App";
-// import App from "./unicafe/App";
-// import App from "./anekdootit/App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
