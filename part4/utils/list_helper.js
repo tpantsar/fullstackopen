55dc53d5c9b36600ed9c9e08081e1051d43bab88
tpantsar <tomi.pantsar@gmail.com>
2024-10-01 11:48:44 +0300

    add tests for favoriteBlog

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (max, blog) => (blog.likes > max.likes ? blog : max),
+    blogs[0]
+  )
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
