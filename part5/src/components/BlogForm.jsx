2709793d961b3585f15620437132f98c9079e28f
Tomi Pantsar <tomi___00@hotmail.com>
2024-10-20 13:28:17 +0300

    add styles to blogForm

--- part5/src/components/BlogForm.jsx
+++ part5/src/components/BlogForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import '../styles/BlogForm.css'
 
 const BlogForm = ({ createBlog }) => {
   const [title, setTitle] = useState('')
@@ -19,31 +20,34 @@ const BlogForm = ({ createBlog }) => {
   }
 
   return (
-    <div>
+    <div className="blog-form-container">
       <h3>New blog</h3>
       <form onSubmit={addBlog}>
         <div>
-          Title
+          <label htmlFor="title">Title</label>
           <input
             type="text"
+            id="title"
             value={title}
             name="Title"
             onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div>
-          Author
+          <label htmlFor="author">Author</label>
           <input
             type="text"
+            id="author"
             value={author}
             name="Author"
             onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
-          Url
+          <label htmlFor="url">Url</label>
           <input
             type="text"
+            id="url"
             value={url}
             name="Url"
             onChange={({ target }) => setUrl(target.value)}
