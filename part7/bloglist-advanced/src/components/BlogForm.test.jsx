4a362f29d319447b3f4456bcfea375ad7cf1b41c
tpantsar <tomi.pantsar@gmail.com>
2024-12-18 10:21:03 +0200

    update test

--- part7/bloglist-advanced/src/components/BlogForm.test.jsx
+++ part7/bloglist-advanced/src/components/BlogForm.test.jsx
@@ -1,22 +1,37 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import store from '../store'
 import BlogForm from './BlogForm'
+import Togglable from './Togglable'
 
 test('<BlogForm /> updates parent state and calls onSubmit', async () => {
   const user = userEvent.setup()
   const mockHandler = vi.fn()
 
-  render(<BlogForm createBlog={mockHandler} />)
+  const blogFormRef = useRef()
+
+  render(
+    <Provider store={store}>
+      <Togglable buttonLabel="New blog" ref={blogFormRef}>
+        <BlogForm ref={blogFormRef} />
+      </Togglable>
+    </Provider>
+  )
+
+  const newBlogButton = screen.getByText('New blog')
+  user.click(newBlogButton)
 
   const title = screen.getByRole('textbox', { name: 'Title' })
   const author = screen.getByRole('textbox', { name: 'Author' })
   const url = screen.getByRole('textbox', { name: 'Url' })
 
-  const createButton = screen.getByText('create')
-
   await user.type(title, 'testing form title')
   await user.type(author, 'testing form author')
   await user.type(url, 'testing form url')
+
+  const createButton = screen.getByText('create')
   await user.click(createButton)
 
   const mockCalls = mockHandler.mock.calls
