dbdcd39fbc1b7f72c0459890219b1beb5a0ce978
tpantsar <tomi___00@hotmail.com>
2025-01-20 15:18:09 +0200

    Extract single Todo in a component

--- part12-containers-applications/todo-app/todo-frontend/src/Todos/List.jsx
+++ part12-containers-applications/todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,54 +1,33 @@
-import React from 'react'
+import Todo from './Todo'
+import PropTypes from 'prop-types'
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
-  const onClickDelete = (todo) => () => {
-    deleteTodo(todo)
-  }
-
-  const onClickComplete = (todo) => () => {
-    completeTodo(todo)
-  }
-
   return (
     <>
       {todos
-        .map((todo) => {
-          const doneInfo = (
-            <>
-              <span>This todo is done</span>
-              <span>
-                <button onClick={onClickDelete(todo)}> Delete </button>
-              </span>
-            </>
-          )
-
-          const notDoneInfo = (
-            <>
-              <span>This todo is not done</span>
-              <span>
-                <button onClick={onClickDelete(todo)}> Delete </button>
-                <button onClick={onClickComplete(todo)}> Set as done </button>
-              </span>
-            </>
-          )
-
-          return (
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                maxWidth: '70%',
-                margin: 'auto',
-              }}
-            >
-              <span>{todo.text}</span>
-              {todo.done ? doneInfo : notDoneInfo}
-            </div>
-          )
-        })
-        .reduce((acc, cur) => [...acc, <hr />, cur], [])}
+        .map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            completeTodo={completeTodo}
+          />
+        ))
+        .reduce((acc, cur, index) => [...acc, <hr key={index} />, cur], [])}
     </>
   )
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      text: PropTypes.string.isRequired,
+      done: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  completeTodo: PropTypes.func.isRequired,
 }
 
 export default TodoList
