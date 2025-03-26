832173972c3032ff268af09885e0282e3d231999
tpantsar <tomi.pantsar@gmail.com>
2025-03-26 09:44:56 +0200

    9.15 Vite app with typescript

--- part9-typescript/patientor-client/src/App.tsx
+++ part9-typescript/patientor-client/src/App.tsx
@@ -1,3 +1,32 @@
+const Header = ({ name }: { name: string }) => {
+  return <h2>{name}</h2>;
+};
+
+const Content = ({ parts }: { parts: Array<{ name: string; exerciseCount: number }> }) => (
+  <div>
+    {parts.map(({ name, exerciseCount }, index) => (
+      <Part key={index} name={name} exercises={exerciseCount} />
+    ))}
+  </div>
+);
+
+const Part = ({ name, exercises }: { name: string; exercises: number }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }: { parts: Array<{ name: string; exerciseCount: number }> }) => {
+  return (
+    <p>
+      <strong>Total exercises:</strong>{' '}
+      {parts.map((part) => part.exerciseCount).reduce((a, b) => a + b, 0)}
+    </p>
+  );
+};
+
 const App = () => {
   const courseName = 'Half Stack application development';
   const courseParts = [
@@ -15,21 +44,11 @@ const App = () => {
     },
   ];
 
-  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
-
   return (
     <div>
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>Number of exercises {totalExercises}</p>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
     </div>
   );
 };
