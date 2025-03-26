4372bdb97107b5bbd7944b389c63cb50600f7c53
tpantsar <tomi.pantsar@gmail.com>
2025-03-26 12:08:35 +0200

    initialize flight-diary server project

---
+++ part9-typescript/flight-diary/server/src/routes/diaries.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response, NextFunction } from 'express';
+import diaryService from '../services/diaryService';
+import { NewEntrySchema } from '../utils';
+
+import { z } from 'zod';
+import { NewDiaryEntry, DiaryEntry } from '../types';
+
+const router = express.Router();
+
+router.get('/', (_req, res: Response<DiaryEntry[]>) => {
+  res.send(diaryService.getNonSensitiveEntries());
+});
+
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    NewEntrySchema.parse(req.body);
+    console.log(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.post(
+  '/',
+  newDiaryParser,
+  (req: Request<unknown, unknown, NewDiaryEntry>, res: Response<DiaryEntry>) => {
+    const addedEntry = diaryService.addDiary(req.body);
+    res.json(addedEntry);
+  },
+);
+
+router.use(errorMiddleware);
+
+export default router;
---
+++ part9-typescript/flight-diary/server/src/utils.ts
@@ -0,0 +1,13 @@
+import { NewDiaryEntry, Weather, Visibility } from './types';
+import { z } from 'zod';
+
+export const NewEntrySchema = z.object({
+  weather: z.nativeEnum(Weather),
+  visibility: z.nativeEnum(Visibility),
+  date: z.string().date(),
+  comment: z.string().optional(),
+});
+
+export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
+  return NewEntrySchema.parse(object);
+};
