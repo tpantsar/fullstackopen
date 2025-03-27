20b32cd6c53eebecdcc5b0dd9b8b7ea472ba1cfd
tpantsar <tomi.pantsar@gmail.com>
2025-03-27 11:37:26 +0200

    add DiaryList component

--- part9-typescript/flight-diary/client/src/App.tsx
+++ part9-typescript/flight-diary/client/src/App.tsx
@@ -1,42 +1,14 @@
-import { Button } from '@mui/material';
-import axios from 'axios';
+import { Button, Container, Divider, Typography } from '@mui/material';
 
 import { useEffect, useState } from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import AddDiaryModal from './components/AddDiaryModal';
+import DiaryList from './components/DiaryList';
 import diaryService from './diaryService';
-import { Diary, DiaryFormValues, Visibility, Weather } from './types';
-
-const DiaryItem = ({ diary }: { diary: Diary }) => (
-  <div className="diary-item">
-    <p>Date: {diary.date}</p>
-    <p>weather: {diary.weather}</p>
-    <p>visibility: {diary.visibility}</p>
-    {diary.comment && <p>Comment: {diary.comment}</p>}
-  </div>
-);
+import { Diary } from './types';
 
 const App = () => {
-  const initialDiaries: Diary[] = [
-    {
-      id: 1,
-      date: '2025-03-27',
-      weather: Weather.Sunny,
-      visibility: Visibility.Great,
-      comment: 'Nice flight!',
-    },
-  ];
-
-  const [diaries, setDiaries] = useState<Diary[]>(initialDiaries);
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [error, setError] = useState<string>();
-
-  const openModal = (): void => setModalOpen(true);
-
-  const closeModal = (): void => {
-    setModalOpen(false);
-    setError(undefined);
-  };
+  const [diaries, setDiaries] = useState<Diary[]>([]);
 
   useEffect(() => {
     diaryService.getAllDiaries().then((data: Diary[]) => {
@@ -44,42 +16,22 @@ const App = () => {
     });
   }, []);
 
-  const submitNewDiary = async (values: DiaryFormValues) => {
-    try {
-      const diary = await diaryService.createNewDiary(values);
-      setDiaries(diaries.concat(diary));
-      setModalOpen(false);
-    } catch (e: unknown) {
-      if (axios.isAxiosError(e)) {
-        if (e?.response?.data && typeof e?.response?.data === 'string') {
-          const message = e.response.data.replace('Something went wrong. Error: ', '');
-          console.error(message);
-          setError(message);
-        } else {
-          setError('Unrecognized axios error');
-        }
-      } else {
-        console.error('Unknown error', e);
-        setError('Unknown error');
-      }
-    }
-  };
-
   return (
-    <div>
-      <AddDiaryModal
-        modalOpen={modalOpen}
-        onSubmit={submitNewDiary}
-        error={error}
-        onClose={closeModal}
-      />
-      <Button variant="contained" onClick={() => openModal()}>
-        New Flight
-      </Button>
-      <h2>Previous flights</h2>
-      {diaries.map((diary) => (
-        <DiaryItem key={diary.id} diary={diary} />
-      ))}
+    <div className="App">
+      <Router>
+        <Container>
+          <Typography variant="h3" style={{ marginBottom: '0.5em' }}>
+            Flight Diary
+          </Typography>
+          <Button component={Link} to="/" variant="contained" color="primary">
+            Home
+          </Button>
+          <Divider hidden />
+          <Routes>
+            <Route path="/" element={<DiaryList diaries={diaries} setDiaries={setDiaries} />} />
+          </Routes>
+        </Container>
+      </Router>
     </div>
   );
 };
---
+++ part9-typescript/flight-diary/client/src/components/DiaryList/index.tsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import axios from 'axios';
+
+import { useState } from 'react';
+
+import diaryService from '../../diaryService';
+import { Diary, DiaryFormValues } from '../../types';
+import AddDiaryModal from '../AddDiaryModal';
+
+interface Props {
+  diaries: Diary[];
+  setDiaries: React.Dispatch<React.SetStateAction<Diary[]>>;
+}
+
+const DiaryList = ({ diaries, setDiaries }: Props) => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
+  const submitNewDiary = async (values: DiaryFormValues) => {
+    try {
+      const diary = await diaryService.createNewDiary(values);
+      setDiaries(diaries.concat(diary));
+      setModalOpen(false);
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        if (e?.response?.data && typeof e?.response?.data === 'string') {
+          const message = e.response.data.replace('Something went wrong. Error: ', '');
+          console.error(message);
+          setError(message);
+        } else {
+          setError('Unrecognized axios error');
+        }
+      } else {
+        console.error('Unknown error', e);
+        setError('Unknown error');
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <Box>
+        <Typography align="center" variant="h6">
+          Previous flights
+        </Typography>
+      </Box>
+      <Button variant="contained" onClick={() => openModal()}>
+        New Flight
+      </Button>
+      <Table style={{ marginBottom: '1em' }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Weather</TableCell>
+            <TableCell>Visibility</TableCell>
+            <TableCell>Comment</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.values(diaries).map((diary: Diary) => (
+            <TableRow key={diary.id}>
+              <TableCell>{diary.date}</TableCell>
+              <TableCell>{diary.weather}</TableCell>
+              <TableCell>{diary.visibility}</TableCell>
+              <TableCell>{diary.comment}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <AddDiaryModal
+        modalOpen={modalOpen}
+        onSubmit={submitNewDiary}
+        error={error}
+        onClose={closeModal}
+      />
+    </div>
+  );
+};
+
+export default DiaryList;
