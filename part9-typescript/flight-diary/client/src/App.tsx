b66d69da871f02aa39c13105fc688eeb7a53bf36
tpantsar <tomi.pantsar@gmail.com>
2025-03-27 09:51:22 +0200

    add css styles to DiaryItem

--- part9-typescript/flight-diary/client/src/App.tsx
+++ part9-typescript/flight-diary/client/src/App.tsx
@@ -3,6 +3,14 @@ import { useEffect, useState } from 'react';
 import { getAllDiaries } from './diaryService';
 import { DiaryEntry, Visibility, Weather } from './types';
 
+const DiaryItem = ({ diary }: { diary: DiaryEntry }) => (
+  <div className="diary-item">
+    <p>Date: {diary.date}</p>
+    <p>weather: {diary.weather}</p>
+    <p>visibility: {diary.visibility}</p>
+  </div>
+);
+
 const App = () => {
   const initialDiaries: DiaryEntry[] = [
     {
@@ -23,12 +31,12 @@ const App = () => {
 
   return (
     <div>
-      <ul>
-        {diaries.map((diary) => (
-          <li key={diary.id}>{diary.date}</li>
-        ))}
-      </ul>
+      <h2>Previous flights</h2>
+      {diaries.map((diary) => (
+        <DiaryItem key={diary.id} diary={diary} />
+      ))}
     </div>
   );
 };
+
 export default App;
